1eb9e6651b223d3515d42bdd66b821f92fd32638
Matt Huntington <matt.huntington@gmail.com>
2020-06-30 14:48:00 -0400

    format time

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 let startTime;
 let windowInterval;
 
+const formatTime = (total) => {
+    const hours = Math.floor(total/60/60);
+    const minutes = Math.floor( (total - (hours*60*60)) / 60 );
+    const seconds = total - minutes*60 - hours * 60 * 60;
+    return `${hours}:${minutes}:${seconds}`;
+}
+
 const displayTime = () => {
     // console.log(Date.now()-startTime);
-    document.querySelector('h1').innerHTML = Math.floor((Date.now()-startTime)/1000);
+    const timeString = formatTime(Math.floor((Date.now()-startTime)/1000));
+    document.querySelector('h1').innerHTML = timeString;
 }
 
 document.querySelector('.btn-primary').addEventListener('click', (event) => {
@@ -15,6 +23,6 @@ document.querySelector('.btn-secondary').addEventListener('click', (event) => {
     window.clearInterval(windowInterval)
 })
 
-window.onbeforeunload = function(){
-    return 'Good bye'
-}
+// window.onbeforeunload = function(){
+//     return 'Good bye';
+// }
